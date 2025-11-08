76903bda0b89021968594d73de781d34f62df786
Sandeep Kumar <sandy.aniyan@gmail.com>
2025-11-09 00:06:56 +0530

    adding chnges to baseurl

--- baseUrl.ts
+++ baseUrl.ts
@@ -1,12 +1,7 @@
-// baseUrl.ts
-const vercelURL =
-  process.env.VERCEL_ENV === "production"
-    ? process.env.VERCEL_PROJECT_PRODUCTION_URL
-    : process.env.VERCEL_BRANCH_URL || process.env.VERCEL_URL;
-
 export const baseURL =
-  process.env.NODE_ENV === "development"
+  process.env.NODE_ENV == "development"
     ? "http://localhost:3000"
-    : vercelURL
-      ? `https://${vercelURL}`
-      : "http://localhost:3000";  // Fallback for local production mode (no Vercel env)
+    : "https://" +
+      (process.env.VERCEL_ENV === "production"
+        ? process.env.VERCEL_PROJECT_PRODUCTION_URL
+        : process.env.VERCEL_BRANCH_URL || process.env.VERCEL_URL);
